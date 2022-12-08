a773b70f292ff9b919dd4a66ffd47db28d8e8a4a
Benoit Técher <techer.benoit2113@gmail.com>
2022-12-08 11:30:11 +0100

    1st commit on develop

--- ProjetPlanningMeteo/src/main/java/dao/AvoirDroitsCrudPlanningAutreUtilisateurJPADAO.java
+++ ProjetPlanningMeteo/src/main/java/dao/AvoirDroitsCrudPlanningAutreUtilisateurJPADAO.java
@@ -22,16 +22,58 @@ public class AvoirDroitsCrudPlanningAutreUtilisateurJPADAO {
 
 
     public Optional<AvoirDroitsCrudPlanningAutreUtilisateur> findByIds(Long idProprietaire, Long idAyantDroit) {
-       return null;
+        EntityManager em = ConnectionManager.getEntityManager();
+        TypedQuery<AvoirDroitsCrudPlanningAutreUtilisateur> query = em.createQuery("select ad from AvoirDroitsCrudPlanningAutreUtilisateur ad where ad.proprietaire.idUtilisateur = " + idProprietaire + " and ad.ayantDroit.idUtilisateur = " + idAyantDroit, AvoirDroitsCrudPlanningAutreUtilisateur.class);
+        List<AvoirDroitsCrudPlanningAutreUtilisateur> ads = query.getResultList();
+        em.close();
+        return ads.stream().findAny();
     }
+    /*public void removeIfUseless(Long idProprietaire, Long idAyantDroit){
+        EntityManager em = ConnectionManager.getEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        Optional<AvoirDroitsCrudPlanningAutreUtilisateur> relation = findByIds(idProprietaire, idAyantDroit);
+        if(relation.isPresent() && !relation.get().getPeutModifier() && !relation.get().getPeutCreer() && !relation.get().getPeutLire() && ! relation.get().getPeutSupprimer()){
+            delete(relation.get());
+        }
+    }*/
 
-
-    public boolean delete(Long id) {
-
-        return false;
-    }
 
     public boolean update(AvoirDroitsCrudPlanningAutreUtilisateur element) {
+        if(!element.getPeutSupprimer() && ! element.getPeutSupprimer() && !element.getPeutLire() && !element.getPeutCreer()){
+            return delete(element);
+        }
+        EntityManager em = ConnectionManager.getEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        Long idProprietaire = element.getProprietaire().getIdUtilisateur();
+        Long idAyantDroit = element.getAyantDroit().getIdUtilisateur();
+        Optional<AvoirDroitsCrudPlanningAutreUtilisateur> relation = findByIds(idProprietaire, idAyantDroit);
+        if(relation.isPresent()){
+            trans.begin();
+            relation.get().setPeutCreer((element.getPeutCreer()));
+            relation.get().setPeutModifier((element.getPeutModifier()));
+            relation.get().setPeutSupprimer((element.getPeutSupprimer()));
+            relation.get().setPeutLire((element.getPeutLire()));
+            trans.commit();
+            em.close();
+            return true;
+        }
+        em.close();
+        return false;
+    }
+    public boolean delete(AvoirDroitsCrudPlanningAutreUtilisateur element){
+        EntityManager em = ConnectionManager.getEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        Long idProprietaire = element.getProprietaire().getIdUtilisateur();
+        Long idAyantDroit = element.getAyantDroit().getIdUtilisateur();
+        Optional<AvoirDroitsCrudPlanningAutreUtilisateur> relation = findByIds(idProprietaire, idAyantDroit);
+        if(relation.isPresent()){
+            trans.begin();
+            em.remove(relation.get());
+            trans.commit();
+            em.close();
+            return true;
+        }
+        em.close();
         return false;
     }
 }
